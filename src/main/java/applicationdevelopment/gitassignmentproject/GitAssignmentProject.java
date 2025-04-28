f66bc90b215ed7229775eefbc30a80e8689c108a
Alpha <francewitness9@gmail.com>
2025-04-28 12:43:00 +0200

    Hello NetBeans. Welcome to version control using Git

---
+++ src/main/java/applicationdevelopment/gitassignmentproject/GitAssignmentProject.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package applicationdevelopment.gitassignmentproject;
+
+/**
+ *
+ * @author HP ELITEBOOK
+ */
+public class GitAssignmentProject {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
